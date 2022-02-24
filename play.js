78c7b418c8778c78bf37141aeea5bf1b6f495b82
Daryan Zycki <da.zycki@gmail.com>
2022-02-24 04:06:19 +0000

    Move inputs and handlers to input.js for better readability.

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const { connect } = require("./client"); // "Pull in" connect function
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', key => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
+const { setupInput } = require("./input"); // "Pull in" setupInput function
 
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+setupInput();
