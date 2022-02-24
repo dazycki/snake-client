cdcdfa8909de6120e9408a0dfb4376498ef7492c
Daryan Zycki <da.zycki@gmail.com>
2022-02-24 03:58:42 +0000

    Add support for stdin and event handler to terminate.

--- play.js
+++ play.js
@@ -1,7 +1,22 @@
-const net = require("net");
-const { connect } = require("./client");
+const { connect } = require("./client"); // "Pull in" connect function
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
 
-
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
