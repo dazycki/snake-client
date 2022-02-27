5635248c0461aa3285f6665df66861edc57f1e44
Daryan Zycki <da.zycki@gmail.com>
2022-02-27 05:05:11 +0000

    Initialize file to store constant values.

---
+++ constants.js
@@ -0,0 +1,23 @@
+// CONST for connection
+const IP = 'localhost';
+const PORT = 50541;
+const initials = 'DNZ';
+
+// CONST for game input
+const UP = 'Move: up';
+const DOWN = 'Move: down';
+const LEFT = 'Move: left';
+const RIGHT = 'Move: right';
+
+const HIGHFIVE = 'Say: Highfive!';
+
+module.exports = {
+  IP,
+  PORT,
+  initials,
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+  HIGHFIVE
+};
