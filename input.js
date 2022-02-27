1a58089974338d0ae2356c526c1ec39a483cb3b0
Daryan Zycki <da.zycki@gmail.com>
2022-02-27 05:07:29 +0000

    Add clear intervals and constants module.

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { UP, DOWN, LEFT, RIGHT, HIGHFIVE } = require("./constants");
+
 // Stores the active TCP connection object.
 let connection;
 
@@ -9,34 +11,66 @@ const setupInput = (conn) => {
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on('data', key => {
-    handleUserInput(key);
+    handleUserInput(key); 
   });
   return stdin;
 };
 
-const handleUserInput = function (key) {
+// define setIntervals as global scope so they can be accessed by clearInterval function
+let wInterval;
+let aInterval;
+let sInterval;
+let dInterval;
+
+const handleUserInput = function(key) {
+  
+  // control to exit game
   if (key === '\u0003') {
     process.exit();
   }
+  
+  // control for emote
   if (key === 'q') {
     console.log('q key was pressed to send a highfive!');
-    connection.write('Say: Highfive!');
+    connection.write(HIGHFIVE);
   }
+
+  // controls for player direction
   if (key === 'w') {
     console.log('W key was pressed to go up!');
-    connection.write('Move: up');
+    clearInterval(aInterval); // clears other ongoing intervals 
+    clearInterval(sInterval);
+    clearInterval(dInterval);
+    wInterval = setInterval(() => {
+      connection.write(UP);
+    }, 50);
   }
   if (key === 's') {
     console.log('s key was pressed to go down!');
-    connection.write('Move: down');
+    clearInterval(wInterval);
+    clearInterval(aInterval);
+    clearInterval(dInterval);
+    sInterval = setInterval(() => {
+      connection.write(DOWN);
+    }, 50);
   }
   if (key === 'a') {
     console.log('a key was pressed to go left!');
-    connection.write('Move: left');
+    clearInterval(wInterval);
+    clearInterval(sInterval);
+    clearInterval(dInterval);
+    aInterval = setInterval(() => {
+      connection.write(LEFT);
+    }, 50);
   }
   if (key === 'd') {
     console.log('d key was pressed to go right!');
-    connection.write('Move: right');
+    clearInterval(wInterval);
+    clearInterval(aInterval);
+    clearInterval(sInterval);
+    dInterval = setInterval(() => {
+      connection.write(RIGHT);
+    }, 50);
   }
 };
 
